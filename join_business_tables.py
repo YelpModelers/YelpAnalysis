20c07de643c88306c7c686af0a3dd3a29bcd2a01
Hayden <hayden@dhcp-10-250-228-255.harvard.edu>
2019-11-13 19:50:01 -0500

    adding the joining of all three datasets

--- join_business_tables.py
+++ join_business_tables.py
@@ -13,7 +13,6 @@ clean_reviews = clean_reviews.rename(columns={'cool_stand':'cool_review_pm', 'fu
                                               'funny':'funny_review_count', 'cool':'cool_review_count',
                                               'review':'review_count', 'checkin':'checkin_count'})
 
-
 clean_users = clean_users.rename(columns={'stars_mean':'stars_review_mean', 'average_stars_mean':'stars_users_mean',
                                           'cool_mean':'cool_users_mean', 'elite_binary_sum':'elite_users_sum',
                                           'fans_sum':'fans_users_sum', 'fans_mean':'fans_users_mean',
@@ -22,3 +21,12 @@ clean_users = clean_users.rename(columns={'stars_mean':'stars_review_mean', 'ave
                                           'useful_mean':'useful_users_mean', 'count_friends_sum':'friend_number_count',
                                           'count_friends_mean':'friend_number_mean', 'reviews_per_month_mean':'reviews_users_pm_mean',
                                           'fan_outlier_sum':'fan_outlier_count','friend_outlier_sum':'friend_outlier_count'})
+
+clean_business = clean_business.rename(columns={'review_count':'review_count_business', 'stars':'stars_business'})
+
+vegas_yelp_dataset = pd.merge(clean_business, clean_reviews, how='left', left_on='business_id', right_on='business_id')
+vegas_yelp_dataset = pd.merge(vegas_yelp_dataset, clean_users, how='left', left_on='business_id', right_on='business_id')
+vegas_yelp_dataset.to_csv('data/vegas_yelp_dataset.csv')
+
+print('Overall columns:')
+print(vegas_yelp_dataset.columns)
